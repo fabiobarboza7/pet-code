5b16fdf8045781aa0614ee2c175b49c7c651ea6b
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-06-13 17:33:12 -0300

    creating some tests

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,11 +1,31 @@
 class Animal < ApplicationRecord
   belongs_to :animal_kind
   belongs_to :person
-  before_save :check_age
+  before_save :check_animal_kind
 
-  def check_age
-  	person = self.person
-	  (person.date_of_birth + 18.years) > Date.today ? true : false
+  def check_animal_kind
+  	case self.animal_kind.kind
+  	when "Andorinha"
+  		check_age_for_bird
+  	when "Gato"
+  		check_name
+  	else
+  		true
+  	end	
+  end
+
+  def check_cost
+  	self.person.animals.sum(:monthly_cost).to_f <= 1000.00 ? true : false
+  end
+
+  private
+
+  def check_age_for_bird
+		(self.person.date_of_birth + 18.years) < Date.today ? true : false
+  end
+
+  def check_name
+  	self.person.name.first.downcase === "a" ? false : true
   end
 
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Person: " do 
+    it "should save andorinha if person > 18" do
+    	person = Person.create(name: "Fábio", date_of_birth: "1900-11-01")
+    	andorinha = AnimalKind.create(kind: "Andorinha")
+      animal = Animal.new(name: "tigre", animal_kind_id: andorinha.id, person_id: person.id)
+      expect(animal.check_animal_kind).to eq true
+    end
+  
+    it "should save andorinha if person < 18" do
+    	person = Person.create(name: "Fábio", date_of_birth: "2015-11-01")
+    	andorinha = AnimalKind.create(kind: "Andorinha")
+      animal = Animal.new(name: "tigre", animal_kind_id: andorinha.id, person_id: person.id)
+      expect(animal.check_animal_kind).to eq false
+    end
+  
+    it "should not save if animal == 'Gato'" do
+    	person = Person.create(name: "Antônio", date_of_birth: "2015-11-01")
+    	cavalo = AnimalKind.create(kind: "Gato")
+      animal = Animal.new(name: "tigre", animal_kind_id: cavalo.id, person_id: person.id)
+      expect(animal.check_animal_kind).to eq false
+    end
+  
+    it "should save anyway if animal isn't == Andorinha" do
+    	person = Person.create(name: "Fábio", date_of_birth: "2015-11-01")
+    	cavalo = AnimalKind.create(kind: "Cavalo")
+      animal = Animal.new(name: "tigre", animal_kind_id: cavalo.id, person_id: person.id)
+      expect(animal.save).to eq true
+    end
+
+    it "should not have more animals if current cost will be > 1000" do
+      person = Person.create(name: "Fábio", date_of_birth: "2015-11-01")
+      cavalo = AnimalKind.create(kind: "Cavalo")
+      animal = Animal.create(name: "tigre", animal_kind_id: cavalo.id, person_id: person.id, monthly_cost: 1001.00)
+      expect(animal.check_cost).to eq false
+    end
+
+    it "should have more animals if current cost will be < 1000" do
+      person = Person.create(name: "Fábio", date_of_birth: "2015-11-01")
+      cavalo = AnimalKind.create(kind: "Cavalo")
+      animal = Animal.create(name: "tigre", animal_kind_id: cavalo.id, person_id: person.id, monthly_cost: 101.00)
+      expect(animal.check_cost).to eq true
+    end
+  end
 end
